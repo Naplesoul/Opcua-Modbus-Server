f932a578a17499f81812b70d8fd2895204c334f1
Naplesoul <shenwhang@outlook.com>
2022-02-05 01:15:35 +0800

    feat: add Server_addModbusVariable

--- UAServer.c
+++ UAServer.c
@@ -3,7 +3,7 @@
  * @Autor: Weihang Shen
  * @Date: 2022-01-26 00:12:29
  * @LastEditors: Weihang Shen
- * @LastEditTime: 2022-02-04 02:07:54
+ * @LastEditTime: 2022-02-05 01:14:28
  */
 
 #include <stdlib.h>
@@ -18,21 +18,43 @@
 
 #define CONFIG_ERROR -1
 #define CONFIG_OK 0
+
+typedef enum
+{
+    MB_COIL_STATUS, MB_INPUT_STATUS, MB_HOLDING_REGISTER, MB_INPUT_REGISTER
+} MB_Variable_Type;
+
+typedef enum
+{
+    MB_BOOLEAN, MB_FLOAT_ABCD, MB_FLOAT_BADC, MB_FLOAT_CDAB, MB_FLOAT_DCBA, MB_UINT16, MB_UINT32
+} MB_Data_Type;
+
 typedef struct
 {
-    modbus_t *modbus_rtu;
+    modbus_t *device;
+    MB_Variable_Type var_type;
+    MB_Data_Type data_type;
     uint32_t mb_machine_address;
     uint32_t mb_register_address;
 
 } Modbus_Access;
 
-enum MB_Register_Type
-{
-    MB_COIL_STATUS, MB_INPUT_STATUS, MB_HOLDING_REGISTER, MB_INPUT_REGISTER
-};
-
+UA_StatusCode Server_init(char *config);
+UA_StatusCode Server_start();
+void Server_beforeRead(UA_Server *server,
+			           const UA_NodeId *session_id, void *session_context,
+                       const UA_NodeId *node_id, void *node_context,
+                       const UA_NumericRange *range, const UA_DataValue *data);
+void Server_afterWrite(UA_Server *server,
+                       const UA_NodeId *session_id, void *session_context,
+                       const UA_NodeId *node_id, void *node_context,
+                       const UA_NumericRange *range, const UA_DataValue *data);
 UA_StatusCode Server_addModbusRTU(cJSON *rtu_config);
-UA_StatusCode Server_addModbusVariable(cJSON *var_config, modbus_t *device);
+UA_StatusCode Server_addModbusVariable(cJSON *var_config, UA_NodeId parent_node_id, modbus_t *device);
+UA_StatusCode Server_setVariableType(cJSON *var_config, Modbus_Access *access, UA_VariableAttributes *var_attr);
+UA_StatusCode Server_setVariableTypeAndInitalValue(cJSON *var_config, Modbus_Access *access, UA_VariableAttributes *var_attr);
+
+/*####################################################################*/
 
 UA_UInt16 port = 4840;
 UA_Boolean running = false;
@@ -46,7 +68,7 @@ UA_StatusCode Server_init(char *config)
     modbus_access_map = Map_new();
 
     if (cJSON_HasObjectItem(config_root, "Port") == 0)
-		return UA_STATUSCODE_BADDECODINGERROR;
+		return CONFIG_ERROR;
 
     port = cJSON_GetObjectItem(config_root, "Port")->valueint;
 
@@ -54,24 +76,23 @@ UA_StatusCode Server_init(char *config)
     ua_server = UA_Server_new();
     UA_ServerConfig_setMinimal(UA_Server_getConfig(ua_server), port, NULL);
 
-    if (cJSON_HasObjectItem(config_root, "AddressSpace") == 0)
-		return UA_STATUSCODE_BADDECODINGERROR;
+    if (cJSON_HasObjectItem(config_root, "ModbusRTUs") == 0)
+		return CONFIG_ERROR;
     
-	cJSON *address_space = cJSON_GetObjectItem(config_root, "AddressSpace");
-	int node_count = cJSON_GetArraySize(address_space);
+	cJSON *modbus_rtus = cJSON_GetObjectItem(config_root, "ModbusRTUs");
+	int rtu_count = cJSON_GetArraySize(modbus_rtus);
 
-	if (node_count <= 0)
-		return UA_STATUSCODE_BADDECODINGERROR;
+	if (rtu_count <= 0)
+		return CONFIG_ERROR;
     
-	for (int i = 0; i < node_count; ++i) {
-        cJSON *config_node = cJSON_GetArrayItem(address_space, i);
-        UA_StatusCode status_code = Server_addNode(config_node, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), NULL);
-        cJSON_Delete(config_node);
-		if (status_code != 0)
-			return status_code;
+	for (int i = 0; i < rtu_count; ++i) {
+        cJSON *rtu_config = cJSON_GetArrayItem(modbus_rtus, i);
+        UA_StatusCode status_code = Server_addNode(rtu_config, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), NULL);
+        cJSON_Delete(rtu_config);
+		if (status_code != 0) return status_code;
 	}
     
-	cJSON_Delete(address_space);
+	cJSON_Delete(modbus_rtus);
 	return 0;
 }
 
@@ -91,7 +112,7 @@ void Server_beforeRead(UA_Server *server,
     u_int16_t new_value;
     // TODO: read value from modbus reg
     Modbus_Access *access = (Modbus_Access *)Map_get(modbus_access_map, UA_NodeId_hash(node_id));
-    modbus_read_registers(access->modbus_rtu, access->mb_register_address, 1, &new_value);
+    modbus_read_registers(access->device, access->mb_register_address, 1, &new_value);
     UA_Double now = new_value;
     UA_Variant_setScalar(&value, &now, &UA_TYPES[UA_TYPES_DOUBLE]);
     UA_Server_writeValue(server, *node_id, value);
@@ -105,7 +126,7 @@ void Server_afterWrite(UA_Server *server,
     double new_value = *(UA_Double *)(data->value.data);
     // TODO: write value to modbus reg
     Modbus_Access *access = (Modbus_Access *)Map_get(modbus_access_map, UA_NodeId_hash(node_id));
-    modbus_write_register(access->modbus_rtu, access->mb_register_address, new_value);
+    modbus_write_register(access->device, access->mb_register_address, new_value);
     return;
 }
 
@@ -145,7 +166,7 @@ UA_StatusCode Server_addModbusRTU(cJSON *rtu_config)
 
         for (int i = 0; i < var_count; ++i) {
             cJSON *var_config = cJSON_GetArrayItem(variables, i);
-            UA_StatusCode status_code = Server_addModbusVariable(var_config, modbus_rtu);
+            UA_StatusCode status_code = Server_addModbusVariable(var_config, this_node_id, modbus_rtu);
             cJSON_Delete(var_config);
             if (status_code < 0) {
                 cJSON_Delete(variables);
@@ -157,7 +178,7 @@ UA_StatusCode Server_addModbusRTU(cJSON *rtu_config)
     return CONFIG_OK;
 }
 
-UA_StatusCode Server_addModbusVariable(cJSON *var_config, modbus_t *device)
+UA_StatusCode Server_addModbusVariable(cJSON *var_config, UA_NodeId parent_node_id, modbus_t *device)
 {
     int ns_index = cJSON_GetObjectItem(var_config, "NodeID_NamespaceIndex")->valueint;
     UA_NodeId this_node_id = UA_NODEID_STRING(ns_index, cJSON_GetObjectItem(var_config, "NodeID_ID")->valuestring);
@@ -167,126 +188,123 @@ UA_StatusCode Server_addModbusVariable(cJSON *var_config, modbus_t *device)
     var_attr.description = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(var_config, "Description")->valuestring);
     var_attr.displayName = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(var_config, "DisplayName")->valuestring);
     
+    Modbus_Access *access = (Modbus_Access *)malloc(sizeof(Modbus_Access));
+    access->device = device;
+    access->mb_machine_address = cJSON_GetObjectItem(var_config, "MB_MachineAddress")->valueint;
+    access->mb_register_address = cJSON_GetObjectItem(var_config, "MB_RegisterAddress")->valueint;
     char *variable_type = cJSON_GetObjectItem(var_config, "VariableType")->valuestring;
+
     if (strcmp(variable_type, "CoilStatus") == 0) {
-        // TODO
+        access->var_type = MB_COIL_STATUS;
+        access->data_type = MB_BOOLEAN;
+        var_attr.dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_BOOLEAN);
+        if (cJSON_HasObjectItem(var_config, "InitialValue")) {
+            UA_Boolean init_value = cJSON_GetObjectItem(var_config, "InitialValue")->valueint;
+            UA_Variant_setScalar(&var_attr.value, &init_value, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        }
+    } else if (strcmp(variable_type, "InputStatus") == 0) {
+        access->var_type = MB_INPUT_STATUS;
+        access->data_type = MB_BOOLEAN;
+        var_attr.dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_BOOLEAN);
+        // TODO: Set variable READ_ONLY
+        // var_attr.userWriteMask = ?;
+    } else if (strcmp(variable_type, "HoldingRegister") == 0) {
+        access->var_type = MB_HOLDING_REGISTER;
+        UA_StatusCode status_code = Server_setVariableTypeAndInitalValue(var_config, access, &var_attr);
+        if (status_code != CONFIG_OK) return status_code;
+    } else if (strcmp(variable_type, "InputRegister") == 0) {
+        access->var_type = MB_INPUT_REGISTER;
+        UA_StatusCode status_code = Server_setVariableType(var_config, access, &var_attr);
+        if (status_code != CONFIG_OK) return status_code;
+        // TODO: Set variable READ_ONLY
+        // var_attr.userWriteMask = ?;
+    } else {
+        return CONFIG_ERROR;
     }
+
+    UA_Server_addVariableNode(ua_server,
+                              this_node_id,
+                              parent_node_id,
+                              ref_node_id,
+                              UA_QUALIFIEDNAME(1, cJSON_GetObjectItem(var_config, "QualifiedName")->valuestring),
+                              UA_NODEID_NULL,
+                              var_attr, NULL, NULL);
+    
+    Map_put(modbus_access_map, UA_NodeId_hash(&this_node_id), access);
+    // binding read & write callback functions
+    UA_ValueCallback callback;
+	callback.onRead = Server_beforeRead;
+	callback.onWrite = Server_afterWrite;
+	UA_Server_setVariableNode_valueCallback(ua_server, this_node_id, callback);
+
+    return CONFIG_OK;
 }
 
-UA_StatusCode Server_addNode(cJSON *config_node, UA_NodeId parent_node_id, modbus_t *parent_rtu)
+UA_StatusCode Server_setVariableType(cJSON *var_config, Modbus_Access *access, UA_VariableAttributes *var_attr)
 {
-    int ns_index = cJSON_GetObjectItem(config_node, "NodeID_NamespaceIndex")->valueint;
-    if (ns_index > (2 << 15) - 1) {
-        // overflow
-        return UA_STATUSCODE_BADOUTOFRANGE;
-    }
+    char *data_type = cJSON_GetObjectItem(var_config, "DataType")->valuestring;
+    if (strcmp(data_type, "FLOAT") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_FLOAT);
 
-    UA_NodeId this_node_id;
-    if (strcmp(cJSON_GetObjectItem(config_node, "NodeID_IDType")->valuestring, "String") == 0) {
-        this_node_id = UA_NODEID_STRING(ns_index, cJSON_GetObjectItem(config_node, "NodeID_ID")->valuestring);
-    } else if (strcmp(cJSON_GetObjectItem(config_node, "NodeID_IDType")->valuestring, "Numeric") == 0) {
-        this_node_id = UA_NODEID_NUMERIC(ns_index, cJSON_GetObjectItem(config_node, "NodeID_ID")->valueint);
+        char *byte_order = cJSON_GetObjectItem(var_config, "ByteOrder")->valuestring;
+        if (strcmp(byte_order, "abcd") == 0) access->data_type = MB_FLOAT_ABCD;
+        else if (strcmp(byte_order, "badc") == 0) access->data_type = MB_FLOAT_BADC;
+        else if (strcmp(byte_order, "cdab") == 0) access->data_type = MB_FLOAT_CDAB;
+        else if (strcmp(byte_order, "dcba") == 0) access->data_type = MB_FLOAT_DCBA;
+        else return CONFIG_ERROR;
+
+    } else if (strcmp(data_type, "UINT16") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_UINT16);
+        access->data_type = MB_UINT16;
+
+    } else if (strcmp(data_type, "UINT32") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_UINT32);
+        access->data_type = MB_UINT32;
+
     } else {
-        return UA_STATUSCODE_BADOUTOFRANGE;
+        return CONFIG_ERROR;
     }
+    return CONFIG_OK;
+}
 
-    UA_NodeId ref_node_id;
-    if (strcmp(cJSON_GetObjectItem(config_node, "ReferenceTypeId")->valuestring, "Organizes") == 0) {
-        ref_node_id = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
-    } else if (strcmp(cJSON_GetObjectItem(config_node, "ReferenceTypeId")->valuestring, "HasComponent") == 0) {
-        ref_node_id = UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT);
-    } else if (strcmp(cJSON_GetObjectItem(config_node, "ReferenceTypeId")->valuestring, "HasProperty") == 0) {
-        ref_node_id = UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY);
-    } else {
-        return UA_STATUSCODE_BADOUTOFRANGE;
-    }
+UA_StatusCode Server_setVariableTypeAndInitalValue(cJSON *var_config, Modbus_Access *access, UA_VariableAttributes *var_attr)
+{
+    char *data_type = cJSON_GetObjectItem(var_config, "DataType")->valuestring;
+    if (strcmp(data_type, "FLOAT") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_FLOAT);
 
-    if (strcmp(cJSON_GetObjectItem(config_node, "NodeClass")->valuestring, "Object") == 0) {
-        if (strcmp(cJSON_GetObjectItem(config_node, "Communication")->valuestring, "Serial") == 0) {
-            parent_rtu = modbus_new_rtu(cJSON_GetObjectItem(config_node, "Device")->valuestring,
-                                        cJSON_GetObjectItem(config_node, "Baud")->valueint,
-                                        cJSON_GetObjectItem(config_node, "Parity")->valuestring[0],
-                                        cJSON_GetObjectItem(config_node, "DataBits")->valueint,
-                                        cJSON_GetObjectItem(config_node, "StopBits")->valueint);
-            modbus_set_slave(parent_rtu, cJSON_GetObjectItem(config_node, "MB_MachineAddress")->valueint);
-            if (strcmp(cJSON_GetObjectItem(config_node, "SerialMode")->valuestring, "RS485") == 0) {
-                modbus_rtu_set_serial_mode(parent_rtu, MODBUS_RTU_RS485);
-            }
-            if (modbus_connect(parent_rtu) < 0) {
-                return -1;
-            }
-        } else {
-            printf("Only support serial devices now...\n");
-            return UA_STATUSCODE_BADDECODINGERROR;
-        }
-        UA_ObjectAttributes obj_attr = UA_ObjectAttributes_default;
-        obj_attr.description = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(config_node, "Description")->valuestring);
-        obj_attr.displayName = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(config_node, "DisplayName")->valuestring);
-        UA_Server_addObjectNode(ua_server,
-                                this_node_id,
-                                parent_node_id,
-                                ref_node_id,
-                                UA_QUALIFIEDNAME(1, cJSON_GetObjectItem(config_node, "QualifiedName")->valuestring),
-                                UA_NODEID_NULL,
-                                obj_attr, NULL, NULL);
-
-    } else if (strcmp(cJSON_GetObjectItem(config_node, "NodeClass")->valuestring, "Variable") == 0) {
-        UA_VariableAttributes var_attr = UA_VariableAttributes_default;
-        var_attr.description = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(config_node, "Description")->valuestring);
-        var_attr.displayName = UA_LOCALIZEDTEXT("en-US", cJSON_GetObjectItem(config_node, "DisplayName")->valuestring);
-
-        if (strcmp(cJSON_GetObjectItem(config_node, "DataType")->valuestring, "Double") == 0) {
-            UA_Double *temp = (UA_Double *)malloc(sizeof(UA_Double));
-			var_attr.dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_DOUBLE);
-			*temp = cJSON_GetObjectItem(config_node, "InitialValue")->valuedouble;
-			UA_Variant_setScalar(&var_attr.value, temp, &UA_TYPES[UA_TYPES_DOUBLE]);
-        } else if (strcmp(cJSON_GetObjectItem(config_node, "DataType")->valuestring, "Int16") == 0) {
-            UA_UInt16 tmp = cJSON_GetObjectItem(config_node, "InitialValue")->valueint;
-			var_attr.dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_UINT16);
-			UA_Variant_setScalar(&var_attr.value, &tmp, &UA_TYPES[UA_TYPES_UINT16]);
-        } else {
-            return UA_STATUSCODE_BADOUTOFRANGE;
-        }
-        UA_Server_addVariableNode(ua_server,
-                                  this_node_id,
-                                  parent_node_id,
-                                  ref_node_id,
-                                  UA_QUALIFIEDNAME(1, cJSON_GetObjectItem(config_node, "QualifiedName")->valuestring),
-                                  UA_NODEID_NULL,
-                                  var_attr, NULL, NULL);
+        char *byte_order = cJSON_GetObjectItem(var_config, "ByteOrder")->valuestring;
+        if (strcmp(byte_order, "abcd") == 0) access->data_type = MB_FLOAT_ABCD;
+        else if (strcmp(byte_order, "badc") == 0) access->data_type = MB_FLOAT_BADC;
+        else if (strcmp(byte_order, "cdab") == 0) access->data_type = MB_FLOAT_CDAB;
+        else if (strcmp(byte_order, "dcba") == 0) access->data_type = MB_FLOAT_DCBA;
+        else return CONFIG_ERROR;
         
-        Modbus_Access *access = (Modbus_Access *)malloc(sizeof(Modbus_Access));
-        access->mb_machine_address = cJSON_GetObjectItem(config_node, "MB_MachineAddress")->valueint;
-        access->mb_register_address = cJSON_GetObjectItem(config_node, "MB_RegisterAddress")->valueint;
-        access->modbus_rtu = parent_rtu;
-
-        Map_put(modbus_access_map, UA_NodeId_hash(&this_node_id), access);
-
-        // binding read & write callback functions
-        UA_ValueCallback callback;
-	    callback.onRead = Server_beforeRead;
-	    callback.onWrite = Server_afterWrite;
-	    UA_Server_setVariableNode_valueCallback(ua_server, this_node_id, callback);
-
-    } else {
-        return UA_STATUSCODE_BADOUTOFRANGE;
-    }
-
-    if (cJSON_HasObjectItem(config_node, "ChildNode")) {
-        cJSON *child_nodes = cJSON_GetObjectItem(config_node, "ChildNode");
-        int node_count = cJSON_GetArraySize(child_nodes);
-
-        for (int i = 0; i < node_count; ++i) {
-            cJSON *child_config_node = cJSON_GetArrayItem(child_nodes, i);
-            UA_StatusCode status_code = Server_addNode(child_config_node, this_node_id, parent_rtu);
-            cJSON_Delete(child_config_node);
-            if (status_code < 0) {
-                return status_code;
-            }
+        if (cJSON_HasObjectItem(var_config, "InitialValue")) {
+            UA_Float init_value = (UA_Float)cJSON_GetObjectItem(var_config, "InitialValue")->valuedouble;
+            UA_Variant_setScalar(&var_attr->value, &init_value, &UA_TYPES[UA_TYPES_FLOAT]);
         }
 
-        cJSON_Delete(child_nodes);
-    }
+    } else if (strcmp(data_type, "UINT16") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_UINT16);
+        access->data_type = MB_UINT16;
 
-    return 0;
+        if (cJSON_HasObjectItem(var_config, "InitialValue")) {
+            UA_UInt16 init_value = (UA_UInt16)cJSON_GetObjectItem(var_config, "InitialValue")->valueint;
+            UA_Variant_setScalar(&var_attr->value, &init_value, &UA_TYPES[UA_TYPES_UINT16]);
+        }
+
+    } else if (strcmp(data_type, "UINT32") == 0) {
+        var_attr->dataType = UA_NODEID_NUMERIC(0, UA_NS0ID_UINT32);
+        access->data_type = MB_UINT32;
+
+        if (cJSON_HasObjectItem(var_config, "InitialValue")) {
+            UA_UInt32 init_value = (UA_UInt32)cJSON_GetObjectItem(var_config, "InitialValue")->valueint;
+            UA_Variant_setScalar(&var_attr->value, &init_value, &UA_TYPES[UA_TYPES_UINT32]);
+        }
+
+    } else {
+        return CONFIG_ERROR;
+    }
+    return CONFIG_OK;
 }
